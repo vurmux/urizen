281a2e3297d27809ad808ff619a9e04931b8cbc8
Andrey Voronov <vurmux@gmail.com>
2019-12-12 19:02:24 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='urizen',
+    version='0.0.1',
+    description='World generation library',
+    long_description='',
+    url='https://github.com/vurmux/urizen',
+    author='Andrey Voronov',
+    author_email='vurmux@gmail.com',
+    license='Apache',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='',
+    packages=find_packages(exclude=['docs', 'img']),
+    install_requires=[],
+    extras_require={
+        'dev': [],
+        'test': ['coverage'],
+    },
+    package_data={},
+    entry_points={},
+)
+
