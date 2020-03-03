157fea8692c18e730d60229958daab55dbd45e54
Andrey Voronov <vurmux@gmail.com>
2020-03-03 19:20:11 +0200

    Add some furniture in thing_collection

--- urizen/core/thing_collection.py
+++ urizen/core/thing_collection.py
@@ -16,3 +16,60 @@ item_skull = type('item_skull', (Thing,), {
     'passable': True,
     'tags': []
 })
+
+# Furniture
+
+furniture_hearth = type('furniture_hearth', (Thing,), {
+    'thing_type': 'furniture',
+    'symbol': '#',
+    'bg_color': '#000000',
+    'fg_color': '#FF0000',
+    'pixel_color': '#B06000',
+    'sprite': None,
+    'passable': False,
+    'tags': []
+})
+
+furniture_table = type('furniture_table', (Thing,), {
+    'thing_type': 'furniture',
+    'symbol': '=',
+    'bg_color': '#000000',
+    'fg_color': '#FF0000',
+    'pixel_color': '#905000',
+    'sprite': None,
+    'passable': False,
+    'tags': []
+})
+
+furniture_alchemist_table = type('furniture_alchemist_table', (Thing,), {
+    'thing_type': 'furniture',
+    'symbol': '=',
+    'bg_color': '#000000',
+    'fg_color': '#00FF00',
+    'pixel_color': '#40B040',
+    'sprite': None,
+    'passable': False,
+    'tags': []
+})
+
+furniture_chair = type('furniture_chair', (Thing,), {
+    'thing_type': 'furniture',
+    'symbol': '\\',
+    'bg_color': '#000000',
+    'fg_color': '#FF0000',
+    'pixel_color': '#905000',
+    'sprite': None,
+    'passable': False,
+    'tags': []
+})
+
+furniture_bookcase = type('furniture_bookcase', (Thing,), {
+    'thing_type': 'furniture',
+    'symbol': '[',
+    'bg_color': '#000000',
+    'fg_color': '#FF0000',
+    'pixel_color': '#FFEED0',
+    'sprite': None,
+    'passable': False,
+    'tags': []
+})
