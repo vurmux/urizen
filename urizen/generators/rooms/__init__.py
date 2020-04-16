7a72ff11fa6224f0be574a456b1a7a574e10a25f
Margarita Potyomkina <misagai@hotmail.com>
2020-04-16 20:43:11 +0300

    Prison cells and simple linear prison generator

--- urizen/generators/rooms/__init__.py
+++ urizen/generators/rooms/__init__.py
@@ -3,3 +3,9 @@ from urizen.generators.rooms.building_housebarn import building_housebarn
 
 import urizen.generators.rooms.building_hunter_robber_house
 from urizen.generators.rooms.building_hunter_robber_house import building_hunter_robber_house
+
+import urizen.generators.rooms.building_prison
+from urizen.generators.rooms.building_prison import building_prison_linear
+
+import urizen.generators.rooms.room_prison_cell
+from urizen.generators.rooms.room_prison_cell import room_prison_cell
