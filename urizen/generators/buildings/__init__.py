b6ac0c1fbdda4b6bf1def0ca3b57490c10ce50ad
Margarita Potyomkina <misagai@hotmail.com>
2020-05-14 20:09:06 +0300

    Create stables building generator

--- urizen/generators/buildings/__init__.py
+++ urizen/generators/buildings/__init__.py
@@ -7,8 +7,6 @@ from urizen.generators.buildings.building_hunter_robber_house import building_hu
 import urizen.generators.buildings.building_prison
 from urizen.generators.buildings.building_prison import building_prison
 
-#import urizen.generators.buildings.building_stables
-#from urizen.generators.buildings.building_stables import building_stables
+import urizen.generators.buildings.building_stables
+from urizen.generators.buildings.building_stables import building_stables
 
-#import urizen.generators.buildings.building_bank
-#from urizen.generators.buildings.building_bank import building_bank
